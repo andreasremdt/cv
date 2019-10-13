d701a6fd8cdee8517d3a760503b36840c3be4cd1
Andreas Remdt <me@andreasremdt.com>
2019-10-13 12:27:33 +0200

    Add i18n

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
+import Translator from "./translator.js";
+
 (function() {
   "use strict";
 
   var menu = document.getElementById('menu');
+  var translator = new Translator();
+  
+  translator.load();
 
   function handlePrintClick() {
     window.print();
@@ -18,21 +23,32 @@
   function handleMenuClose(evt) {
     if (evt.target.dataset.action === "toggle-menu") {
       return;
-    } else if (!evt.target.closest("#menu") && !menu.hasAttribute("hidden")) {
+    }
+    
+    if (!evt.target.closest("#menu") && !menu.hasAttribute("hidden")) {
       menu.setAttribute("hidden", true);
     }
   }
 
   function handleKeyDown(evt) {
-    if (menu.hasAttribute("hidden")) {
-      return;
-    } else if (evt.key === "Escape") {
+    if (!menu.hasAttribute("hidden") && evt.key === "Escape") {
       menu.setAttribute("hidden", true);
+    }
+  }
+
+  function handleFormClick(evt) {
+    var input = evt.target;
+
+    if (input.tagName === "INPUT" && input.name === "lang") {
+      translator.load(input.value);
+
+      localStorage.setItem("language", input.value);
     }
   }
 
   document.querySelector('[data-action="toggle-print"]').addEventListener("click", handlePrintClick);
   document.querySelector('[data-action="toggle-menu"]').addEventListener("click", handleMenuClick);
+  document.querySelector('[data-action="form"]').addEventListener("click", handleFormClick);
   document.body.addEventListener("click", handleMenuClose);
   window.addEventListener("keydown", handleKeyDown);
 })();
---
+++ js/translator.js
@@ -0,0 +1,53 @@
+"use strict";
+
+class Translator {
+  constructor() {
+    this._lang = this.getLanguage();
+    this._elements = document.querySelectorAll("[data-i18n]");
+  }
+
+  getLanguage() {
+    if (localStorage.getItem("language")) {
+      return localStorage.getItem("language");
+    }
+    
+    var lang = navigator.languages ? navigator.languages[0] : navigator.language;
+    
+    if (/\w{2}-\w{2}/i.test(lang)) {
+      return lang.split("-")[0];
+    }
+    
+    return lang;
+  }
+
+  load(lang = null) {
+    if (lang) {
+      this._lang = lang;
+    }
+  
+    import(`../i18n/${this._lang}.js`).then((resources) => {
+      this.translate(resources.default);
+      this.toggleInput();
+    }).catch(() => {
+      console.error(`Could not load "${this._lang}.js". Please make sure that the path is correct.`);
+    });
+  }
+  
+  translate(translation) {
+    function replace(element) {
+      var text = element.dataset.i18n.split('.').reduce((obj, i) => obj[i], translation);
+  
+      if (text) {
+        element.innerHTML = text;
+      }
+    }
+  
+    this._elements.forEach(replace);
+  }
+
+  toggleInput() {
+    document.querySelector(`input[type="radio"][value="${this._lang}"]`).checked = true;
+  }
+}
+
+export default Translator;
